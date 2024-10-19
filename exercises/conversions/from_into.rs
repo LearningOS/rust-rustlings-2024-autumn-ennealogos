d840b9c141f3e4c08255fc62b24ee0825ef57c1d
ennealogos <ennealogos@outlook.com>
2024-10-19 12:08:59 +0800

    working on conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        //检查字符串是否为空
+        if s.is_empty() {
+            return Person::default();
+        }
+        //分割字符串
+        let parts: Vec<&str> = s.split(",").collect();
+        //检查输入是否正确
+        if parts.len() != 2 || parts[0].is_empty() {
+            return Person::default();
+        }
+        //返回正确的名字与年龄
+        let name = parts[0].to_string();
+        match parts[1].parse::<usize>() {
+            Ok(age) => Person{name, age},
+            Err(_) => Person::default(),
+        }
+
     }
 }
 
